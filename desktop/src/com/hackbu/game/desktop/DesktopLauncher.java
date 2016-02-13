9377df87b5d2aa4e6e54713cf1944c51b8047988
Anthony Muratore <amurato1@binghamton.edu>
2016-02-13 17:05:33 -0500

    change resolution

--- desktop/src/com/hackbu/game/desktop/DesktopLauncher.java
+++ desktop/src/com/hackbu/game/desktop/DesktopLauncher.java
@@ -8,6 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "test1";
+        config.width = 800;
+        config.height = 480;
 		new LwjglApplication(new HackBU(), config);
 	}
 }
