fb932e7a799100ea432bcf898752815020ff200e
kbaker8 <gilly2312@gmail.com>
2016-02-13 17:50:28 -0500

    test

--- desktop/src/com/hackbu/game/desktop/DesktopLauncher.java
+++ desktop/src/com/hackbu/game/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.hackbu.game.HackBU;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "test1";
+		config.title = "test2";
         config.width = 800;
         config.height = 600;
 		new LwjglApplication(new HackBU(), config);
