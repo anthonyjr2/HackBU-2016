4dfa99e6499b6f2ace179bd0e6a6077c609d9555
Anthony Muratore <amurato1@binghamton.edu>
2016-02-13 15:54:19 -0500

    Initial commit

---
+++ desktop/src/com/hackbu/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hackbu.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hackbu.game.HackBU;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HackBU(), config);
+	}
+}
