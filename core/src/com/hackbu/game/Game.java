b26f14820182d8b480a677019ed361afbc82f7e9
MetalAirship <andy.oliver.262@gmail.com>
2016-02-13 20:46:27 -0500

    Merge branch 'master' of /home/mlh-user/proj/hackbu-2016 with conflicts.

--- core/src/com/hackbu/game/Game.java
+++ core/src/com/hackbu/game/Game.java
@@ -6,29 +6,35 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Game extends ApplicationAdapter {
 	private SpriteBatch batch;
-	Texture backgroundImage;
+	Texture background;
 	private OrthographicCamera camera;
     private Texture playerImage;
+    private Texture backTex1;
+    private Rectangle back1;
+    private Texture back2;
     private Rectangle player;
     private int xRes = 800;
     private int yRes = 600;
-    private Rectangle background;
+    private int backSpeed = 10;
+    private int backX = 0;
+
 
 	@Override
 	public void create () {
-		backgroundImage = new Texture("gamelevel1base.png");
-        background = new Rectangle();
-        background.x = 0;
-        background.y =0;
+		//background = new Texture("clouds2.jpg");
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, xRes, yRes);
         batch = new SpriteBatch();
         playerImage = new Texture("player.png");
+        backTex1 = new Texture("gamelevel1base.png");
+        back1 = new Sprite(backTex1);
+        back2 = new Texture("gamelevel1base 2.png");
         player = new Rectangle();
         player.x = xRes/2 - 64/2;
         player.y = 80;
@@ -44,18 +50,16 @@ public class Game extends ApplicationAdapter {
         camera.update();
         batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-        batch.draw(backgroundImage, background.x, background.y);
         batch.draw(playerImage, player.x, player.y);
+        batch.draw(back1,800,0);
         batch.end();
-
-        int i = 0;
-        while(i<10){
-            background.x -= 10 * Gdx.graphics.getDeltaTime();
-            i++;
-        }
 
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) player.x -= 200 * Gdx.graphics.getDeltaTime();
         if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) player.x += 200 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.X)){
+            back1.translateX(1);
+        }
+
 
         if(player.x < 0) player.x = 0;
         if(player.x > 800 - 64) player.x = 800 - 64;
