b331de9918e71f9fd3a36e95a91cf101ded0c99c
Anthony Muratore <amurato1@binghamton.edu>
2016-02-13 22:12:43 -0500

    scrolling background simple

--- core/src/com/hackbu/game/Game.java
+++ core/src/com/hackbu/game/Game.java
@@ -2,7 +2,6 @@ package com.hackbu.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -12,25 +11,23 @@ import com.badlogic.gdx.graphics.Texture.TextureWrap;
 
 public class Game extends ApplicationAdapter {
 	private SpriteBatch batch;
-	Texture backgroundImage1;
+	private Texture bgImage;
 	private OrthographicCamera camera;
     private Texture playerImage;
     private Rectangle player;
     private int xRes = 800;
     private int yRes = 600;
-    private Rectangle background1;
-    float backgroundXval1 =0;
+    private int speed;
 
 	@Override
 	public void create () {
-		backgroundImage1 = new Texture("gamelevel1base.png");
-        background1 = new Rectangle();
-        background1.x = 0;
-        background1.y =0;
-        backgroundImage1.setWrap(TextureWrap.Repeat, TextureWrap.Repeat);
+		bgImage = new Texture("gamelevel1base.png");
+
+
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, xRes, yRes);
         batch = new SpriteBatch();
+
         playerImage = new Texture("player.png");
         player = new Rectangle();
         player.x = xRes/2 - 64/2;
@@ -41,28 +38,18 @@ public class Game extends ApplicationAdapter {
 	}
 
 	@Override
-	public void render () {
+	public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         camera.update();
         batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-        batch.draw(backgroundImage1, backgroundXval1, background1.y);
-        backgroundXval1 -=5;
-       
+
+        batch.draw(bgImage, 0, 0, speed, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        speed +=5;
+
         batch.draw(playerImage, player.x, player.y);
         batch.end();
 
-      /*  int i = 0;
-        while(i<10){
-            background.x -= 10 * Gdx.graphics.getDeltaTime();
-            i++;
-        }*/
-
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) player.x -= 200 * Gdx.graphics.getDeltaTime();
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) player.x += 200 * Gdx.graphics.getDeltaTime();
-
-        if(player.x < 0) player.x = 0;
-        if(player.x > 800 - 64) player.x = 800 - 64;
 	}
 }
