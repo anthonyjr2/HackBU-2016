5ca05429adbb24bd18de27e49d6aadee794be057
Anthony Muratore <amurato1@binghamton.edu>
2016-02-13 20:20:50 -0500

    collisions framework

---
+++ core/src/com/hackbu/game/Collisions.java
@@ -0,0 +1,12 @@
+package com.hackbu.game;
+
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+
+public class Collisions extends Game {
+    Engine engine;
+    Entity entity = new Entity();
+
+
+
+}
