58cbdf0b644ce7a62ce2d81550e3a5657db2d1ed
Anthony Muratore <amurato1@binghamton.edu>
2016-02-13 19:07:31 -0500

    resolution change + player entity beginnings

--- core/src/com/hackbu/game/Game.java
+++ core/src/com/hackbu/game/Game.java
@@ -6,35 +6,40 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	SpriteBatch batch2;
+	private SpriteBatch batch;
 	Texture background;
-	Texture img;
-	OrthographicCamera camera;
-    Texture playerImage;
+	private OrthographicCamera camera;
+    private Texture playerImage;
+    private Rectangle player;
+    private int xRes = 1280;
+    private int yRes = 720;
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		background = new Texture("clouds2.jpg");
+		//background = new Texture("clouds2.jpg");
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 600);
-
+		camera.setToOrtho(false, xRes, yRes);
+        batch = new SpriteBatch();
         playerImage = new Texture("player.png");
+        player = new Rectangle();
+        player.x = xRes/2;
+        player.y = 40;
+        player.width = 64;
+        player.height = 64;
 
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 10, 0);
-		batch.draw(background, 10, 0);
-        batch.draw(playerImage,10, 0);
+        batch.draw(playerImage, player.x, player.y);
         batch.end();
 	}
 }
